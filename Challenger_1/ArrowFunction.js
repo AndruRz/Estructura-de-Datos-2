f41b05783aa844661d1a56629bf9ef0696b77e55
AndreRodriguez <Pablorodriguezperez69@gmail.com>
2026-02-05 05:26:49 -0500

    Challenger 1 - Estructura de Datos

---
+++ Challenger_1/ArrowFunction.js
@@ -0,0 +1,10 @@
+const parOImparArrow = (numero) => {
+  if (numero % 2 === 0) {
+    console.log("El número es par");
+  } else {
+    console.log("El número es impar");
+  }
+}
+
+parOImparArrow(10);
+parOImparArrow(3);
